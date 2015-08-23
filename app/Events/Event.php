500c810c04aa662fc325b5afe844d8318e5584ae
Sherif Mesallam <SherifMesallam@gmail.com>
2015-08-23 17:43:54 +0200

    Init

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LaravelBuilder\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaravelBuilder\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
